acb0ca4f77e279a0216ef527032ff97db8271369
David <davidscicua314gmail.com>
2022-12-16 13:47:30 -0500

    Commit

---
+++ Ej1OpenB/src/main/java/co/edu/unab/ejercicio1open/Coche.java
@@ -0,0 +1,10 @@
+
+package co.edu.unab.ejercicio1open;
+
+public class Coche {
+    int N_puertas=0;
+    
+    public void suma_Puertas(){
+        this.N_puertas ++;
+    }
+}
---
+++ Ej1OpenB/src/main/java/co/edu/unab/ejercicio1open/Ejercicio1Open.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package co.edu.unab.ejercicio1open;
+
+/**
+ *
+ * @author ACER
+ */
+public class Ejercicio1Open {
+
+    public static void main(String[] args) {
+      Coche miCoche = new Coche();
+        miCoche.suma_Puertas();
+        miCoche.suma_Puertas();
+        miCoche.suma_Puertas();
+        miCoche.suma_Puertas();
+        .suma_Puertas();
+        c.suma_Puertas();
+        System.out.println(c.N_puertas);
+    }
+
+    public static void sumar(int n1, int n2, int n3) {
+        System.out.println("El resultado es " + (n1 + n2 + n3));
+    }
+    
+}
+
